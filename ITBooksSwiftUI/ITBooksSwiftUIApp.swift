b2502237459727ac7463b9828a4bbc91f5ef15bd
meschya <meschyaaa@gmail.com>
2022-03-23 16:26:32 +0300

    Initial Commit

---
+++ ITBooksSwiftUI/ITBooksSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITBooksSwiftUIApp.swift
+//  ITBooksSwiftUI
+//
+//  Created by Egor Mesheryakov on 23.03.22.
+//
+
+import SwiftUI
+
+@main
+struct ITBooksSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
